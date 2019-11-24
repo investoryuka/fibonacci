12c8f9c8c2b406130a8a82bb38b235c6dccbd836
invetoryuka <investoryuka05232013@outlook.jp>
2019-11-24 13:22:18 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
 function fib(n) {
+    if(memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n,value);
+    return value;
+}
+const length = 100;
+for (let i = 0; i <= length; i++) {
+    Console.log(i + '番目：' + fib(i));
+}
+
+/*function fib(n) {
     if (n === 0){
         return 0;
     } else if (n === 1){
@@ -11,7 +27,7 @@ function fib(n) {
 const length = 42;
 for (let i = 0; i <= length; i++) {
    console.log( i + '番目：' + (fib(i)));
-}
+}*/
 //console.log(fib(0));
 //console.log(fib(1));
 //console.log(fib(2));
